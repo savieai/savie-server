8520bb303f26b45ae016e7fbd26cb88f10083676
Sumair Hassan <sumairhw@gmail.com>
2024-07-05 02:51:57 +0500

    feat: make separate columns for attachments & add search endpoint

--- src/messages/messageHandlers.js
+++ src/messages/messageHandlers.js
@@ -1,18 +1,18 @@
 import express from "express";
-import { createMessage, getMessages } from "./messageService.js";
+import { createMessage, getMessages, searchMessages } from "./messageService.js";
 
 function getMessageRoutes() {
   const router = express.Router();
   router.get("/", index);
   router.post("/", create);
+  router.get("/search", search);
   return router;
 }
 
 async function index(req, res) {
   const { currentUser } = res.locals;
-  const searchTerm = req.query.search;
 
-  const { data, error } = await getMessages(currentUser.sub, searchTerm);
+  const { data, error } = await getMessages(currentUser.sub);
   if (error) {
     return res.status(400).json(error);
   }
@@ -21,16 +21,36 @@ async function index(req, res) {
 }
 
 async function create(req, res) {
-  const text = req.body.text;
-  const attachments = req.body.attachments;
+  const text_content = req.body.text_content;
+  const file_attachments = req.body.file_attachments;
+  const images = req.body.images;
+  const voice_message_url = req.body.voice_message_url;
   const { currentUser } = res.locals;
 
-  const { error } = await createMessage(currentUser.sub, text, attachments);
+  const { error } = await createMessage({
+    userId: currentUser.sub,
+    text_content,
+    file_attachments,
+    images,
+    voice_message_url,
+  });
   if (error) {
     return res.status(400).json(error);
   }
 
   res.status(200).json({ message: "ok" });
+}
+
+async function search(req, res) {
+  const keyword = req.query.q;
+  const type = req.query.type;
+  const { currentUser } = res.locals;
+
+  const { data, error } = await searchMessages({ userId: currentUser.sub, keyword, type });
+  if (error) {
+    return res.status(400).json(error);
+  }
+  return res.status(200).json(data);
 }
 
 export default getMessageRoutes;
--- src/messages/messageService.js
+++ src/messages/messageService.js
@@ -1,22 +1,111 @@
 import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SECRET_KEY);
 
-export async function getMessages(userId, searchTerm) {
-  let query = supabase.from("messages").select().eq("user_id", userId);
+export async function getMessages(userId) {
+  const { data, error } = await supabase
+    .from("messages")
+    .select(`*, links(url), attachments(attachment_type, name, url)`)
+    .eq("user_id", userId);
 
-  if (searchTerm) {
-    searchTerm = searchTerm.split(" ").join("+");
-    query.textSearch("text", searchTerm);
+  console.log({
+    data: data,
+  });
+
+  return { data, error };
+}
+
+export async function createMessage({
+  userId,
+  text_content,
+  file_attachments,
+  images,
+  voice_message_url,
+}) {
+  let links = extractLinks(text_content);
+  const { data, error: messageError } = await supabase
+    .from("messages")
+    .upsert({
+      user_id: userId,
+      text_content: text_content,
+      voice_message_url: voice_message_url,
+    })
+    .select("id")
+    .single();
+
+  if (messageError) {
+    return { error: messageError };
   }
+
+  const messageId = data.id;
+  const linksData = transformLinks({ messageId, links });
+  const attachmentsData = mergeAttachments({ messageId, images, file_attachments });
+
+  const [{ error: linksError }, { error: attachmentsError }] = await Promise.all([
+    supabase.from("links").insert(linksData),
+    supabase.from("attachments").insert(attachmentsData),
+  ]);
+
+  if (linksError) return { error: linksError };
+  if (attachmentsError)
+    return {
+      error: attachmentsError,
+    };
+
+  return { messageId };
+}
+
+export async function searchMessages({ userId, keyword, type }) {
+  if (!keyword) {
+    return [];
+  }
+
+  keyword = keyword.split(" ").join("+");
+  let searchColumn = "text_content";
+  let table = "messages";
+
+  switch (type) {
+    case "image":
+    case "file":
+      searchColumn = "name";
+      table = "attachments";
+      break;
+    case "link":
+      table = "links";
+      searchColumn = "url";
+      break;
+  }
+
+  let query = supabase.from(table).select();
+  if (type === "image" || type === "file") {
+    query.eq("attachment_type", type);
+  }
+  query.textSearch(searchColumn, keyword);
 
   const { data, error } = await query;
   return { data, error };
 }
 
-export async function createMessage(userId, text, attachments) {
-  const { error } = await supabase
-    .from("messages")
-    .insert({ user_id: userId, text: text, attachments: attachments });
+function extractLinks(text) {
+  const linkRegex = /\b(?:https?:\/\/)?(?:www\.)?(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(?:\/[^\s]*)?/g;
+  return text.match(linkRegex) || [];
+}
 
-  return { error };
+function transformLinks({ links, messageId }) {
+  return links.map((link) => ({
+    url: link,
+    message_id: messageId,
+  }));
+}
+
+function mergeAttachments({ messageId, images = [], file_attachments = [] }) {
+  const attachments = [
+    ...images.map((image) => ({ ...image, attachment_type: "image", message_id: messageId })),
+    ...file_attachments.map((file) => ({
+      ...file,
+      attachment_type: "file",
+      message_id: messageId,
+    })),
+  ];
+
+  return attachments;
 }
